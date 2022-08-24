32eba80cfd4813d4320216d35498ffdedc3fa118
Jorge <jorgefprietol@gmail.com>
2022-08-23 22:12:59 -0500

    cambiando el puerto por config Eureka CLIENT

--- src/main/java/com/microservicio/jfpl/bookingmicroservicio/BookingMicroservicioApplication.java
+++ src/main/java/com/microservicio/jfpl/bookingmicroservicio/BookingMicroservicioApplication.java
@@ -2,8 +2,10 @@ package com.microservicio.jfpl.bookingmicroservicio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BookingMicroservicioApplication {
 
 	public static void main(String[] args) {
