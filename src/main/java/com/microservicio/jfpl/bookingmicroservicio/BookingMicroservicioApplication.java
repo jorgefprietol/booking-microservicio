9a84545b955f89ee0c025686ed0602d2a61d66e4
Jorge <jorgefprietol@gmail.com>
2022-08-23 21:27:28 -0500

    inicio

---
+++ src/main/java/com/microservicio/jfpl/bookingmicroservicio/BookingMicroservicioApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio.jfpl.bookingmicroservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingMicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingMicroservicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservicio/jfpl/bookingmicroservicio/BookingMicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.jfpl.bookingmicroservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingMicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
